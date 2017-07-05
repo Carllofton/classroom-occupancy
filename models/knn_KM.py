050f4c4f961e471ea45bc258c98255be188875a8
Kristen McIntyre <Kautumn06@users.noreply.github.com>
2017-07-05 17:18:02 -0400

    Add updated machine learning models

--- models/knn_KM.py
+++ models/knn_KM.py
@@ -7,9 +7,10 @@ import requests
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import TimeSeriesSplit
-from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import LabelEncoder, RobustScaler
 
 # Data ingestion
 URL = 'https://raw.githubusercontent.com/georgetown-analytics/classroom-occupancy/master/models/sensor_data_ml.csv'
@@ -24,7 +25,7 @@ def fetch_data(fname='sensor_data_ml.csv'):
 
 DATA = fetch_data()
 
-# Load sensor data as pandas dataframe with DateTimeIndex: df
+# Load sensor data as pandas dataframe with DateTimeIndex
 df = pd.read_csv('sensor_data_ml.csv', index_col='datetime', parse_dates=True)
 
 # Encode multiclass target variable
@@ -36,16 +37,25 @@ X = df.drop('occupancy_level', axis=1).values
 y = df['occupancy_level']
 
 # Use TimeSeriesSplit to create training and test indices
-tscv = TimeSeriesSplit(n_splits=12)
+tscv = TimeSeriesSplit(n_splits=5)
 for train_index, test_index in tscv.split(X):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
-# Fit GaussianNB classifier to the training data: bayes
-bayes = GaussianNB().fit(X_train, y_train)
+# Create pipeline to scale the data and tune the model's hyperparameter: pipeline
+pipeline = make_pipeline(RobustScaler(), KNeighborsClassifier())
+
+# Setup parameter grid of possible n_neighbors values: param_grid
+param_grid = {'kneighborsclassifier__n_neighbors': np.arange(1, 15)}
+
+# Use GridSearchCV to find optimum number of n_neighbors: knn
+knn = GridSearchCV(pipeline, param_grid=param_grid, cv=tscv)
+
+# Fit the GridSearchCV object to the training data
+knn.fit(X_train, y_train)
 
 # Predict test set labels: y_pred
-y_pred = bayes.predict(X_test)
+y_pred = knn.predict(X_test)
 
 # Save fitted model to disk
-pickle.dump(bayes, open(bayes_model, 'wb'))
+pickle.dump(knn, open(knn_model, 'wb'))
--- models/logreg_KM.py
+++ models/logreg_KM.py
@@ -1,14 +1,16 @@
 # Import models
-from urllib.request import urlretrieve
+import os
+import json
+import time
+import pickle
+import requests
 import numpy as np
 import pandas as pd
-import pickle
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
 from sklearn.preprocessing import LabelEncoder, RobustScaler
-
 
 # Data ingestion
 URL = 'https://raw.githubusercontent.com/georgetown-analytics/classroom-occupancy/master/models/sensor_data_ml.csv'
@@ -23,7 +25,7 @@ def fetch_data(fname='sensor_data_ml.csv'):
 
 DATA = fetch_data()
 
-# Load sensor data as pandas dataframe with DateTimeIndex
+# Load sensor data as pandas dataframe with DateTimeIndex: df
 df = pd.read_csv('sensor_data_ml.csv', index_col='datetime', parse_dates=True)
 
 # Encode multiclass target variable
@@ -40,15 +42,15 @@ for train_index, test_index in tscv.split(X):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
-# Create pipeline to scale the data and tune the model's hyperparameter
+# Create pipeline to scale the data and tune the model's hyperparameter: pipe
 pipe = make_pipeline(RobustScaler(), LogisticRegression(solver='lbfgs', multi_class='multinomial'))
 
 param_grid = {'logisticregression__C': [0.01, 0.1, 1, 10, 100, 110, 120]}
 
-logreg_cv = GridSearchCV(pipe, param_grid, cv=tscv)
+grid = GridSearchCV(pipe, param_grid, cv=tscv)
 
-# Fit the pipeline to the training set: logreg_cv
-logreg_clf = logreg_cv.fit(X_train, y_train)
+# Fit the pipeline to the training data: logreg_clf
+logreg_clf = grid.fit(X_train, y_train)
 
 # Predict the test set labels: y_pred
 y_pred = logreg_clf.predict(X_test)
@@ -57,4 +59,4 @@ y_pred = logreg_clf.predict(X_test)
 logreg_model = 'logreg_model.sav'
 
 # Save fitted model to disk
-pickle.dump(logreg, open(logreg_model, 'wb'))
+pickle.dump(logreg_clf, open(logreg_model, 'wb'))
